9765259738a8c008f1e42fec1f8da3f33993b98c
llm <luis.longo.m@gmail.com>
2024-04-20 10:14:25 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# Description
+
+- app.py
+includes the backend of the application - all the routes and request logic.
+
+- grocery_list.db
+a complete SQLite groceries database which was created with grocery_list.py
+
+- grocery_list.py
+the file which created grocery_list.db
+
+- templates/index.html
+The HTML template of the application, containing Jinja expressions
+
+- static/css/main.css
+the stylesheet of the application
+
+- static/js/main.js
+includes all the client side interactions of the app
+
+
+# References
+
+- https://www.youtube.com/watch?v=v3CSQkPJtAc
+- https://github.com/MariyaSha/groceriesList
---
+++ completeApp/static/js/main.js
@@ -0,0 +1,20 @@
+// It changes the submit button in "remove_items" from "CHECK ITEMS" to "REMOVE ITEMS" when at least 1 iteam is selected,
+// and also the text decoration
+function check_me(input_id){
+    var checked_input = document.querySelector("input[id=" + input_id + "]");
+    var checked_label = document.querySelector("label[name=" + input_id + "]");
+
+    if (checked_input.checked){
+        checked_label.style.textDecoration = "line-through";
+    }
+    else {
+         checked_label.style.textDecoration = "";
+    }
+
+    var btn = document.getElementById("remove_btn");
+
+    btn.value = "REMOVE ITEMS";
+    btn.style.color = "#FFFFFF";
+    btn.style.backgroundColor = "#FE7575";
+    btn.style.cursor = "pointer";
+}
